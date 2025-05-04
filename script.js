5ad23a160fb4748c50099dcd61a12fcc84ac69d8
Akhil Pandey <akhilpandey0002@gmail.com>
2025-05-04 23:03:47 +0530

    script.js

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-// const apiKey = "7d5e74e7b112e34001dc87b79a2fc7c3";
-// const apiUrl = "https://api.openweathermap.org/data/2.5.weather?units=metric&q=";
-// const searchbox = document.querySelector(".search input");
-// const searchbtn = document.querySelector(".search button");
-// const weatherIcon = document.querySelector(".weather-icon");
+const apiKey = "7d5e74e7b112e34001dc87b79a2fc7c3";
+const apiUrl = "https://api.openweathermap.org/data/2.5.weather?units=metric&q=";
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-// async function checkWeather(city) {
-//     const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
 
-//     if(response.status == 404){
-//         document.querySelector(".error").style.display = "block";
-//         document.querySelector(".weather").style.display = "none";
-//     }else{
-//         var data = await response.json();
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
+        var data = await response.json();
 
-//         document.querySelector("city").innerHTML = data.name;
-//         document.querySelector("temp").innerHTML = Math.round(data.main.temp) + "*C";
-//         document.querySelector(".humidity").innerHTML = data.wind.speed + "km/h";
+        document.querySelector("city").innerHTML = data.name;
+        document.querySelector("temp").innerHTML = Math.round(data.main.temp) + "*C";
+        document.querySelector(".humidity").innerHTML = data.wind.speed + "km/h";
 
-//         if (data.weather[0].main == "Clouds"){
-//             weatherIcon.src = "img/clouds.png";
-//         }else if(data.weather[0].main == "Clear"){
-//             weatherIcon.src = "img/clear.png";
-//         }else if(data.weather[0].main == "Rain"){
-//                 weatherIcon.src = "img/rain.png";
-//         }else if(data.weather[0].main == "Drizzle"){
-//             weatherIcon.src = "img/drizzle.png";
-//         }else if(data.weather[0].main == "Mist"){
-//             weatherIcon.src = "img/mist.png";
-//         }
+        if (data.weather[0].main == "Clouds"){
+            weatherIcon.src = "img/clouds.png";
+        }else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "img/clear.png";
+        }else if(data.weather[0].main == "Rain"){
+                weatherIcon.src = "img/rain.png";
+        }else if(data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "img/drizzle.png";
+        }else if(data.weather[0].main == "Mist"){
+            weatherIcon.src = "img/mist.png";
+        }
 
-//         document.querySelector("weather").style.display="block";
-//         document.querySelector(".error").style.display="none";
-//     }
+        document.querySelector("weather").style.display="block";
+        document.querySelector(".error").style.display="none";
+    }
 
     
-// }
-// searchbtn.addEventListener("click",() =>{
-//     checkWeather(searchbox.value);
-// });
+}
+searchbtn.addEventListener("click",() =>{
+    checkWeather(searchbox.value);
+});
 
-// checkWeather();
+checkWeather();
